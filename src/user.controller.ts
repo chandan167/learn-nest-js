70b48a352e60fdaccd50cac6b10d9da4f7efb283
chandan singh <chandansingh16794@gmail.com>
2022-12-19 20:23:23 +0530

    add status-code, header and redirect

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,14 +1,54 @@
-import { Controller, Get, Req } from "@nestjs/common";
-import { Request } from "express";
+import { Controller, Get, Req, HttpCode, HttpStatus, Res, Header, Redirect } from "@nestjs/common";
+import { Request, Response } from "express";
 
 @Controller('/users')
 export class UserController{
 
 
     @Get('/profile')
-    getProfile(@Req() req: Request){
+    @HttpCode(HttpStatus.NO_CONTENT)
+    @Header('Cache-Control', 'none')
+    @Header('X-Name', 'Chandan')
+    getProfile(@Req() req: Request, @Res({passthrough: true}) res: Response){
+        res.status(200)
         return {
             name: "Chandan Singh"
+        }
+    }
+
+
+    @Get('/dynamic/:id')
+    dynamicRoute(@Req() req: Request, @Res({passthrough: true}) res: Response){
+        res.status(200)
+        return req.params
+    }
+
+
+    @Get('/wallet')
+    @Redirect('/users/profile')
+    wallet(){
+        return {
+            message: "wallet route"
+        }
+    }
+
+
+    @Get('/account')
+    @Redirect()
+    account(@Req() req: Request, @Res({passthrough: true}) res: Response){
+        res.status(200)
+        const random = (Math.random() * 10 + 1)
+
+        if(random < 5){
+            return {
+                url: '/users/profile',
+                statusCode: 302
+            }
+        }else{
+            return {
+                url: `/users/dynamic/${random}`,
+                statusCode: 302
+            }
         }
     }
 }
